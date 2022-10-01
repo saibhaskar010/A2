48de9c57e70fc6dd56350b220d6452ddb7a8c97c
COE-LOAN-039\coe_user <bhaskar.sai.x01@gmail.com>
2022-10-01 03:05:43 -0700

    2nd commit

--- assign.py
+++ assign.py
@@ -1,1 +1,18 @@
-import numpy
+from flask import Flask
+import requests
+import pprint
+r =requests.get('https://api.openweathermap.org/data/2.5/weather?q=Corvallis,us&APPID=0d4c3c05756767f0202df9dd82e9d402')
+pprint.pprint(r.text)
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return r.text
+ 
+# main driver function
+if __name__ == '__main__':
+ 
+    # run() method of Flask class runs the application
+    # on the local development server.
+    app.run()
